e3ce1e2f95b9b7e9152c6b6c30bddea4338fd2c7
Mickey_zhaang <qzhang27@colby.edu>
2025-04-25 23:00:52 -0400

    fixed issue where I didn't set a secret key in conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,6 +15,7 @@ def app(monkeypatch):
     app.config["TESTING"] = True
     app.config["OPENAI_API_KEY"] = "dummy_key"
     app.config["PROPAGATE_EXCEPTIONS"] = False
+    app.config["SECRET_KEY"] = "test-secret-key"
     return app
 
 
